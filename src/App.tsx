9db1698ef9c14bf0ae29b1428c829e2cf38598a4
LeeJS9856 <wnstj1057@naver.com>
2026-08-13 02:08:37 +0900

    feat: 광고 촬영 안내 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Archive from '@/pages/Archive/Archive';
+import Capture from '@/pages/Capture/Capture';
 import Create from '@/pages/Create/Create';
 import Home from '@/pages/Home/Home';
 
@@ -9,6 +10,7 @@ const App = (): React.JSX.Element => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create" element={<Create />} />
+      <Route path="/create/capture" element={<Capture />} />
       <Route path="/archive" element={<Archive />} />
     </Routes>
   </BrowserRouter>
